b4b25c820c14a3f62b0950dd1be5466701f16169
Jonny-Cho <andante2183@gmail.com>
2021-05-25 20:20:08 +0900

    chore: change to static variables

--- src/main/java/com/shorturl/shorturl/util/RandomString.java
+++ src/main/java/com/shorturl/shorturl/util/RandomString.java
@@ -1,5 +1,6 @@
 package com.shorturl.shorturl.util;
 
+import com.shorturl.shorturl.exception.RandomStringLengthException;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.context.annotation.ScopedProxyMode;
@@ -9,28 +10,26 @@ import java.security.SecureRandom;
 import java.util.Objects;
 import java.util.Random;
 
+import static com.shorturl.shorturl.exception.RandomStringLengthException.PLEASE_INPUT_MORE_THAN_THREE_SYMBOLS;
+import static com.shorturl.shorturl.exception.RandomStringLengthException.PLEASE_INPUT_MORE_THAN_TWO_CHARACTERS;
+
 @Component
 @Scope(value = ConfigurableBeanFactory.SCOPE_PROTOTYPE, proxyMode = ScopedProxyMode.TARGET_CLASS)
 public class RandomString {
-
-    public String nextString() {
-        for (int idx = 0; idx < buf.length; ++idx) {
-            buf[idx] = symbols[random.nextInt(symbols.length)];
-        }
-        return new String(buf);
-    }
 
     private static final String upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     private static final String lower = upper.toLowerCase();
     private static final String digits = "0123456789";
     private static final String alphanum = upper + lower + digits;
+    private static final int MIN_RANDOM_VALUE = 5;
+    private static final int MAX_RANDOM_VALUE = 8;
+
     private final Random random;
     private final char[] symbols;
     private final char[] buf;
-
     private RandomString(final int length, final Random random, final String symbols) {
-        if (length < 1) throw new IllegalArgumentException("2글자 이상의 길이를 입력하세요.");
-        if (symbols.length() < 2) throw new IllegalArgumentException("3글자 이상의 심볼을 입력하세요.");
+        if (length < 1) throw new RandomStringLengthException(PLEASE_INPUT_MORE_THAN_TWO_CHARACTERS);
+        if (symbols.length() < 2) throw new RandomStringLengthException(PLEASE_INPUT_MORE_THAN_THREE_SYMBOLS);
         this.random = Objects.requireNonNull(random);
         this.symbols = symbols.toCharArray();
         this.buf = new char[length];
@@ -45,11 +44,18 @@ public class RandomString {
     }
 
     public RandomString() {
-        this(randomBetween(5, 8));
+        this(randomBetween(MIN_RANDOM_VALUE, MAX_RANDOM_VALUE));
     }
 
     private static int randomBetween(final int min, final int max) {
         return new Random().nextInt(max - min + 1) + min;
+    }
+
+    public String nextString() {
+        for (int idx = 0; idx < buf.length; ++idx) {
+            buf[idx] = symbols[random.nextInt(symbols.length)];
+        }
+        return new String(buf);
     }
 
 }
