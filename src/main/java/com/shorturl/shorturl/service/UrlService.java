2f0940198e7d54327a88a17a27ed24c7d6f26d33
Jonny-Cho <andante2183@gmail.com>
2021-05-25 20:07:39 +0900

    refactor: add validation in UrlService

--- src/main/java/com/shorturl/shorturl/service/UrlService.java
+++ src/main/java/com/shorturl/shorturl/service/UrlService.java
@@ -6,23 +6,25 @@ import com.shorturl.shorturl.repository.UrlRepository;
 import com.shorturl.shorturl.util.RandomString;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
 import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
+@Transactional(readOnly = true)
 public class UrlService {
 
+    public static final String HTTPS = "https://";
+    public static final String HTTP = "http://";
+    public static final String EMPTY_STRING = "";
     private final UrlRepository urlRepository;
     private final RandomString randomString;
 
     @Transactional
     public String generateShortUrl(final UrlRequestDto urlRequest) {
-        // TODO validation
-
+        if(isBlank(urlRequest.getOriginalUrl())) throw new IllegalArgumentException("originalUrl을 입력해주세요");
         final String replacedUrl = removeHttps(urlRequest.getOriginalUrl());
-
         final Optional<Url> OptionalUrl = urlRepository.findByOriginalUrl(replacedUrl);
         if (OptionalUrl.isPresent()) {
             final Url url = OptionalUrl.get();
@@ -32,8 +34,19 @@ public class UrlService {
         return newShortenedUrl(replacedUrl);
     }
 
+    public String getOriginalUrlByShortUrl(final String shortUrl) {
+        return urlRepository.findByShortenedUrl(shortUrl)
+                .map(Url::getOriginalUrl)
+                .orElse(null);
+    }
+
+    private boolean isBlank(final String originalUrl) {
+        final String trimedUrl = originalUrl.trim();
+        return trimedUrl.isEmpty();
+    }
+
     private String removeHttps(final String originalUrl) {
-        return originalUrl.replace("https://", "").replace("http://", "");
+        return originalUrl.replace(HTTPS, EMPTY_STRING).replace(HTTP, EMPTY_STRING);
     }
 
     private String newShortenedUrl(final String replacedUrl) {
@@ -51,11 +64,5 @@ public class UrlService {
             url = urlRepository.findByShortenedUrl(shortenedUrl);
         } while (url.isPresent());
         return shortenedUrl;
-    }
-
-    public String getOriginalUrlByShortUrl(final String shortUrl) {
-        return urlRepository.findByShortenedUrl(shortUrl)
-                .map(Url::getOriginalUrl)
-                .orElse(null);
     }
 }
